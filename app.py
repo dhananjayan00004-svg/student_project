73433595ddfebef1797c4f7249f143b94c6d944e
dhananjayan00004-svg <dhananjayan00004@gmail.com>
2026-09-06 08:09:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,9 +5,20 @@ def main():
     init_db()
     st.title("login page")
     choice = st.selectbox("Choose an option", ["Login", "Create Account"])
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-    st.button("login")
+    if choice == "Create Account":
+        st.subheader("Create New Account")
+        new_username = st.text_input("New Username")
+        new_password = st.text_input("New Password", type="password")
+        if st.button("Create Account"):
+            # Here you would add logic to create the account in the database
+            st.success(f"Account created for {new_username}!")
+    elif choice == "Login":
+        st.subheader("Login")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
+        if st.button("Login"):
+            # Here you would add logic to authenticate the user
+            st.success(f"Logged in as {username}!")
 
 if __name__ == "__main__":
     main()
