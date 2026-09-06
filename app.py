f656cad0549172458a240f183d3ae5681eacf412
dhananjayan00004-svg <dhananjayan00004@gmail.com>
2026-09-06 07:58:56 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from db_helper import init_db
 def main():
     init_db()
     st.title("login page")
-    
+    choice = st.selectbox("Choose an option", ["Login", "Create Account"])
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    st.button("login")
+
 if __name__ == "__main__":
     main()
